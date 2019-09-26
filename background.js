d1cce13ed353c9346fe3ed0b2788e853e17766e7
Alex K <arkarkark@users.noreply.github.com>
2019-09-26 11:24:11 -0700

    standard formatting

--- background.js
+++ background.js
@@ -1,17 +1,19 @@
 // Copyright 2012 wtwf.com, All Rights Reserved.
 // Created by: Alex K (wtwf.com)
+/* global chrome, localStorage */
 
-// TODO(ark) allow updating of patterns to batter ones.
-// TODO(ark) we can expand short urls too!?
-// TODO(ark) have a toolbar icon to turn this on and off
-// TODO(ark) allow credit card looking numbers only for a short period of time?
+// TODO allow updating of patterns to batter ones.
+// TODO we can expand short urls too!?
+// TODO have a toolbar icon to turn this on and off
+// TODO allow credit card looking numbers only for a short period of time?
+// TODO allow you to send clipboard contents to other machines logged in with this user.
 
-// NOTODO(ark) should we do http://amzn.com/ASIN ? not until it supports https
+// NOTODO should we do http://amzn.com/ASIN ? not until it supports https
 
 // The icons are creative commons (non-commercial) licensed from:
 // http://www.ilmarin.info/gallery/album128/scissors01
 
-copyclipper = {};  // Namespace
+var copyclipper = {} // Namespace
 
 /** search and replace regexes
  *  !search!replace!regex_options
@@ -21,171 +23,171 @@ copyclipper.DEFAULT_REGEXES = [
   '# Add all new patterns above this line.',
   '# Begin Default Patterns',
   '# If you don\'t want one of thise patterns to execute, add a # at the start',
-  '!^(http.*)\\?feat=directlink$!$1!',  // picasaweb
-  '!^(http.*)\\?source=rss$!$1!',  // sentinel
-  '!^(http.*)\\?_r=1$!$1!',  // nytimes
-  '!^(http.*)\\?utm_source=[^#]*!$1!',  // GA
-  '!^http.*[/\.]youtube\..*[&?]v=([^&]*).*?(#t=.*)?$!https://youtu.be/$1$2!',
-  '!^https?([a-z0-9:/.]*[/\.]amazon\.[^/]*/' +
+  '!^(http.*)\\?feat=directlink$!$1!', // picasaweb
+  '!^(http.*)\\?source=rss$!$1!', // sentinel
+  '!^(http.*)\\?_r=1$!$1!', // nytimes
+  '!^(http.*)\\?utm_source=[^#]*!$1!', // GA
+  '!^http.*[/\\.]youtube\\..*[&?]v=([^&]*).*?(#t=.*)?$!https://youtu.be/$1$2!',
+  '!^https?([a-z0-9:/.]*[/\\.]amazon\\.[^/]*/' +
     '(gp/product|([^/]*/)?dp)/[^/]*).*$!' +
     'https$1!',
   '!^http.*google.*search.*[?&]tbm=isch&q=([^&]*).*$!' +
     'https://www.google.com/search?tbm=isch&q=$1!',
-  '!^(https?://www.ebay.com/itm/[^/]+/\d+).*!$1!',
+  '!^(https?://www.ebay.com/itm/[^/]+/\\d+).*!$1!',
   '# Uncomment this next line (remove the #) if you feel brave!',
   '#!/index\\.(php|s?html|asp)$!/!',
-  '# End Default Patterns',
-];
+  '# End Default Patterns'
+]
 
 /** How long to wait before the Chrome notification disappears. */
-copyclipper.NOTIFICATION_TIMEOUT = 4000;
+copyclipper.NOTIFICATION_TIMEOUT = 4000
 
 /** Is an object empty.
  * @param {Object} obj the object to check.
  * @return {bool} true if object has properties.
  */
-copyclipper.isEmpty = function(obj) {
+copyclipper.isEmpty = function (obj) {
   for (var i in obj) {
-    if (obj.hasOwnProperty(i)) {
-      return false;
+    if (Object.prototype.hasOwnProperty.call(obj, i)) {
+      return false
     }
   }
-  return true;
-};
+  return true
+}
 
-copyclipper.restoreRegexes = function() {
+copyclipper.restoreRegexes = function () {
   // try and get the data from chrome.storage.sync
-  chrome.storage.sync.get('regexes', function(reply) {
-    var regexes;
+  chrome.storage.sync.get('regexes', function (reply) {
+    var regexes
     if (copyclipper.isEmpty(reply)) {
-      // maybe they are in localstorage?
-      if (localStorage.hasOwnProperty('copyclipper.regexes') &&
+      // maybe they are in localStorage?
+      if (Object.prototype.hasOwnProperty.call(localStorage, 'copyclipper.regexes') &&
           localStorage['copyclipper.regexes']) {
-        console.log('loaded regexes from localStorage');
-        regexes = localStorage['copyclipper.regexes'];
+        console.log('loaded regexes from localStorage')
+        regexes = localStorage['copyclipper.regexes']
       } else {
-        console.log('no regexes found, using defaults');
-        regexes = copyclipper.DEFAULT_REGEXES.join('\n');
+        console.log('no regexes found, using defaults')
+        regexes = copyclipper.DEFAULT_REGEXES.join('\n')
       }
-      chrome.storage.sync.set({regexes: regexes}, function() {
-        console.log('regexes saved to chrome.storage.sync');
-      });
+      chrome.storage.sync.set({ regexes: regexes }, function () {
+        console.log('regexes saved to chrome.storage.sync')
+      })
     } else {
-      console.log('loaded regexes from chrome.storage.sync');
-      regexes = reply['regexes'];
+      console.log('loaded regexes from chrome.storage.sync')
+      regexes = reply.regexes
     }
-    copyclipper.patterns = copyclipper.decodeRegexes(regexes);
-  });
-};
+    copyclipper.patterns = copyclipper.decodeRegexes(regexes)
+  })
+}
 
-copyclipper.decodeRegexes = function(regex_blob) {
-  var regex_lines = regex_blob.split('\n');
-  var patterns = [];
-  for (var i = 0; i < regex_lines.length; i++) {
-    var pattern = copyclipper.decodeRegexLine(regex_lines[i]);
+copyclipper.decodeRegexes = function (regexBlob) {
+  var regexLines = regexBlob.split('\n')
+  var patterns = []
+  for (var i = 0; i < regexLines.length; i++) {
+    var pattern = copyclipper.decodeRegexLine(regexLines[i])
     if (pattern) {
-      patterns.push(pattern);
+      patterns.push(pattern)
     }
   }
-  return patterns;
-};
+  return patterns
+}
 
 /** Remove whitespace from the start & end of the line. */
-copyclipper.trim = function(s) {
-  return s.replace(/^\s+/, '').replace(/\s+$/, '');
-};
+copyclipper.trim = function (s) {
+  return s.replace(/^\s+/, '').replace(/\s+$/, '')
+}
 
 /** Take a line like !search!replace!re_args and turn it into a
  * Two element [RegExp, string] Array.
  * Returns null on comment lines or an error parsing.
  */
-copyclipper.decodeRegexLine = function(regex_line) {
-  console.log(regex_line);
+copyclipper.decodeRegexLine = function (regexLine) {
+  console.log(regexLine)
 
-  regex_line = copyclipper.trim(regex_line);
-  if (regex_line.charAt(0) === '#') {
+  regexLine = copyclipper.trim(regexLine)
+  if (regexLine.charAt(0) === '#') {
     // ignore comment lines
-    return null;
+    return null
   }
 
-  var old_parts = regex_line.split('!');
-  var pos = 1;
-  var parts = [];
+  var oldParts = regexLine.split('!')
+  var pos = 1
+  var parts = []
 
-  while (pos < old_parts.length) {
-    parts.push(old_parts[pos++]);
-    while (old_parts[pos - 1].charAt(old_parts[pos - 1].length - 1) == '\\' &&
-           pos < old_parts.length) {
-      parts[paths.length - 1] = parts[paths.length - 1] + '!' + old_parts[pos];
-      pos++;
+  while (pos < oldParts.length) {
+    parts.push(oldParts[pos++])
+    while (oldParts[pos - 1].charAt(oldParts[pos - 1].length - 1) === '\\' &&
+           pos < oldParts.length) {
+      parts[parts.length - 1] = parts[parts.length - 1] + '!' + oldParts[pos]
+      pos++
     }
   }
-  if (parts.length == 3) {
-    return [RegExp(parts[0], parts[2]), parts[1]];
+  if (parts.length === 3) {
+    return [RegExp(parts[0], parts[2]), parts[1]]
   }
-  return null;
-};
+  return null
+}
 
-copyclipper.textToHtml = function(txt) {
-  txt = txt.replace('&', '&amp;');
-  txt = txt.replace('<', '&lt;');
-  txt = txt.replace('>', '&gt;');
-  return txt;
-};
+copyclipper.textToHtml = function (txt) {
+  txt = txt.replace('&', '&amp;')
+  txt = txt.replace('<', '&lt;')
+  txt = txt.replace('>', '&gt;')
+  return txt
+}
 
-copyclipper.getClipboardContents = function() {
-  var el = document.getElementById('txt');
-  el.focus();
-  el.select();
-  document.execCommand('paste');
-  return el.value;
-};
+copyclipper.getClipboardContents = function () {
+  var el = document.getElementById('txt')
+  el.focus()
+  el.select()
+  document.execCommand('paste')
+  return el.value
+}
 
-copyclipper.setClipboardContents = function(val) {
-  var el = document.getElementById('txt');
-  el.value = val;
-  el.focus();
-  el.select();
-  document.execCommand('copy');
-  copyclipper.clipboardValue = val;
-};
+copyclipper.setClipboardContents = function (val) {
+  var el = document.getElementById('txt')
+  el.value = val
+  el.focus()
+  el.select()
+  document.execCommand('copy')
+  copyclipper.clipboardValue = val
+}
 
 /** Go through all our patterns and sear/replace on val. */
-copyclipper.replace = function(val, patterns) {
+copyclipper.replace = function (val, patterns) {
   for (var i = 0; i < patterns.length; i++) {
-    var search = patterns[i][0];
-    var replace = patterns[i][1];
-    val = val.replace(search, replace);
+    var search = patterns[i][0]
+    var replace = patterns[i][1]
+    val = val.replace(search, replace)
   }
-  return val;
-};
+  return val
+}
 
 /** Called when the clipboard has changed, see if we can clip the clipboard. */
-copyclipper.copyclip = function() {
-  var val = copyclipper.getClipboardContents();
-  var orig = val + '';
+copyclipper.copyclip = function () {
+  var val = copyclipper.getClipboardContents()
+  var orig = val + ''
 
-  val = copyclipper.replace(val, copyclipper.patterns);
+  val = copyclipper.replace(val, copyclipper.patterns)
 
-  copyclipper.clipboardValue = val;
-  copyclipper.clipboardValueOriginal = orig;
-  if (val != orig) {
-    copyclipper.setClipboardContents(val);
-    copyclipper.createNotification();
+  copyclipper.clipboardValue = val
+  copyclipper.clipboardValueOriginal = orig
+  if (val !== orig) {
+    copyclipper.setClipboardContents(val)
+    copyclipper.createNotification()
   }
-};
+}
 
 /** Called every second to see if the clipboard has changed. */
-copyclipper.pollClipboard = function() {
-  var val = copyclipper.getClipboardContents();
-  if (val != copyclipper.clipboardValue) {
-    copyclipper.copyclip();
+copyclipper.pollClipboard = function () {
+  var val = copyclipper.getClipboardContents()
+  if (val !== copyclipper.clipboardValue) {
+    copyclipper.copyclip()
   }
-};
+}
 
-copyclipper.restoreRegexes();
-copyclipper.clipboardValue = '';  // the last value we saw
-copyclipper.clipboardValueOriginal = '';
+copyclipper.restoreRegexes()
+copyclipper.clipboardValue = '' // the last value we saw
+copyclipper.clipboardValueOriginal = ''
 
 /*
 navigator.permissions.query({
@@ -202,65 +204,64 @@ navigator.permissions.query({
 */
 
 function getChromeVersion () {
-  var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
-  return raw ? parseInt(raw[2], 10) : false;
+  var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./)
+  return raw ? parseInt(raw[2], 10) : false
 }
 
 if (getChromeVersion() > 69 && ((chrome.clipboard || {}).onClipboardDataChanged || {}).addEventListener) {
-  console.log("FINALLY USING NEW onClipboardDataChanged API!")
+  console.log('FINALLY USING NEW onClipboardDataChanged API!')
   // use new api https://developer.chrome.com/apps/clipboard
   chrome.clipboard.onClipboardDataChanged.addListener(copyclipper.pollClipboard)
 } else {
   // Check the clipboard every second, if anything changed copyclip/filter it.
-  copyclipper.intervalId = window.setInterval(copyclipper.pollClipboard, 1000);
+  copyclipper.intervalId = window.setInterval(copyclipper.pollClipboard, 1000)
 }
 
-copyclipper.notificationTimer = null;
+copyclipper.notificationTimer = null
 
-copyclipper.createNotification = function() {
-
-  var desciption = '\n\nClipped from:\n';
+copyclipper.createNotification = function () {
+  var desciption = '\n\nClipped from:\n'
   if (copyclipper.clipboardValueOriginal.length <
       copyclipper.clipboardValue.length) {
-    desciption = '\n\nUnclipped from:\n';
+    desciption = '\n\nUnclipped from:\n'
   }
 
-  copyclipper.clearNotifications();
+  copyclipper.clearNotifications()
 
   chrome.notifications.create('',
-                              {
-                                type: 'basic',
-                                title: 'Copyclipper',
-                                message: (copyclipper.clipboardValue +
+    {
+      type: 'basic',
+      title: 'Copyclipper',
+      message: (copyclipper.clipboardValue +
                                           desciption +
                                           copyclipper.clipboardValueOriginal),
-                                iconUrl: 'icon48.png',
-                                buttons: [{title: 'Undo'}]},
-                              function(notificationId) {});
+      iconUrl: 'icon48.png',
+      buttons: [{ title: 'Undo' }]
+    },
+    function (notificationId) {})
 
   if (copyclipper.notificationTimer != null) {
-    clearTimeout(copyclipper.notificationTimer);
+    clearTimeout(copyclipper.notificationTimer)
   }
   copyclipper.notificationTimer = setTimeout(copyclipper.clearNotifications,
-                                             copyclipper.NOTIFICATION_TIMEOUT);
+    copyclipper.NOTIFICATION_TIMEOUT)
+}
 
-};
-
-copyclipper.clearNotifications = function() {
-  chrome.notifications.getAll(function(notificationIds) {
+copyclipper.clearNotifications = function () {
+  chrome.notifications.getAll(function (notificationIds) {
     for (var notificationId in notificationIds) {
-      chrome.notifications.clear(notificationId, function(wasCleared) {});
+      chrome.notifications.clear(notificationId, function (wasCleared) {})
     }
-  });
-};
+  })
+}
 
-copyclipper.notificationButtonClicked = function(notificationId, buttonIndex) {
-  var orig = copyclipper.clipboardValueOriginal;
-  copyclipper.clipboardValueOriginal = copyclipper.clipboardValue;
-  copyclipper.setClipboardContents(orig);
-  copyclipper.createNotification();
-};
+copyclipper.notificationButtonClicked = function (notificationId, buttonIndex) {
+  var orig = copyclipper.clipboardValueOriginal
+  copyclipper.clipboardValueOriginal = copyclipper.clipboardValue
+  copyclipper.setClipboardContents(orig)
+  copyclipper.createNotification()
+}
 
 // handlers for notification clicks
 chrome.notifications.onButtonClicked.addListener(
-  copyclipper.notificationButtonClicked);
+  copyclipper.notificationButtonClicked)
--- options.js
+++ options.js
@@ -1,183 +1,183 @@
 // Copyright 2012 wtwf.com, All Rights Reserved.
 // Created by: Alex K (wtwf.com)
+/* global chrome */
 
-ccoptions = {};
-copyclipper = chrome.extension.getBackgroundPage().copyclipper;
+var ccoptions = {}
+var copyclipper = chrome.extension.getBackgroundPage().copyclipper
 
 // TODO(ark) allow subscriptions to sets of patterns
 // TODO(ark) turn the patterns area into rich text with color coding
 // TODO(ark) display parsing errors
 
-ccoptions.currentTab = 'patterns';
+ccoptions.currentTab = 'patterns'
 
-ccoptions.saveRegexes = function() {
-  var tab = document.getElementById('regexes');
-  chrome.storage.sync.set({regexes: tab.value}, function() {
-    copyclipper.restoreRegexes();
+ccoptions.saveRegexes = function () {
+  var tab = document.getElementById('regexes')
+  chrome.storage.sync.set({ regexes: tab.value }, function () {
+    copyclipper.restoreRegexes()
 
     // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.innerHTML = 'Saved.';
-    setTimeout(function() {
-      status.innerHTML = '';
-    }, 750);
-  });
-};
-
+    var status = document.getElementById('status')
+    status.innerHTML = 'Saved.'
+    setTimeout(function () {
+      status.innerHTML = ''
+    }, 750)
+  })
+}
 
 // Restores select box state to saved value passed in
-ccoptions.restoreRegexes = function(regexes) {
+ccoptions.restoreRegexes = function (regexes) {
   if (!regexes) {
-    return;
+    return
   }
-  var ta = document.getElementById('regexes');
-  ta.value = regexes;
-};
+  var ta = document.getElementById('regexes')
+  ta.value = regexes
+}
 
-ccoptions.addButtons = function(tr) {
-  var td = document.createElement('td');
-  var but = document.createElement('button');
-  but.onclick = function() {
-    ccoptions.removeRow(tr);
-  };
-  but.appendChild(document.createTextNode('X'));
-  td.appendChild(but);
-  tr.appendChild(td);
-};
+ccoptions.addButtons = function (tr) {
+  var td = document.createElement('td')
+  var but = document.createElement('button')
+  but.onclick = function () {
+    ccoptions.removeRow(tr)
+  }
+  but.appendChild(document.createTextNode('X'))
+  td.appendChild(but)
+  tr.appendChild(td)
+}
 
-ccoptions.addRow = function(tr, pattern) {
-  var td = document.createElement('td');
-  var inp = document.createElement('input');
-  inp.value = pattern.search.source;
-  inp.className = 'search';
-  td.appendChild(inp);
-  tr.appendChild(td);
-  td = document.createElement('td');
-  inp = document.createElement('input');
-  inp.value = pattern.replace;
-  inp.className = 'replace';
-  td.appendChild(inp);
-  tr.appendChild(td);
-  ccoptions.addButtons(tr);
-};
+ccoptions.addRow = function (tr, pattern) {
+  var td = document.createElement('td')
+  var inp = document.createElement('input')
+  inp.value = pattern.search.source
+  inp.className = 'search'
+  td.appendChild(inp)
+  tr.appendChild(td)
+  td = document.createElement('td')
+  inp = document.createElement('input')
+  inp.value = pattern.replace
+  inp.className = 'replace'
+  td.appendChild(inp)
+  tr.appendChild(td)
+  ccoptions.addButtons(tr)
+}
 
-ccoptions.getRow = function(tr) {
-  var td = tr.childNodes[0];
-  var inp = td.firstChild;
-  if (inp.nodeName != 'INPUT') {
-    return null;
+ccoptions.getRow = function (tr) {
+  var td = tr.childNodes[0]
+  var inp = td.firstChild
+  if (inp.nodeName !== 'INPUT') {
+    return null
   }
 
-  var search = {};
-  search.source = inp.value;
-  search.global = false;
-  search.ignoreCase = false;
-  search.lastIndex = false;
-  search.multiline = false;
+  var search = {}
+  search.source = inp.value
+  search.global = false
+  search.ignoreCase = false
+  search.lastIndex = false
+  search.multiline = false
 
-  var td = tr.childNodes[1];
-  var inp = td.firstChild;
-  var replace = inp.value;
+  td = tr.childNodes[1]
+  inp = td.firstChild
+  var replace = inp.value
 
-  return {search: search, replace: replace};
-};
+  return { search: search, replace: replace }
+}
 
-ccoptions.removeRow = function(tr) {
-  tr.parentNode.removeChild(tr);
-};
-
+ccoptions.removeRow = function (tr) {
+  tr.parentNode.removeChild(tr)
+}
 
 ccoptions.emptySearch = {
   global: false,
   ignoreCase: false,
   lastIndex: false,
   multiline: false,
-  source: ''};
+  source: ''
+}
 
-ccoptions.add = function() {
-  var tab = document.getElementById('patterns');
-  var tr = document.createElement('tr');
-  ccoptions.addRow(tr, {search: ccoptions.emptySearch, replace: ''});
-  tab.appendChild(tr);
-};
+ccoptions.add = function () {
+  var tab = document.getElementById('patterns')
+  var tr = document.createElement('tr')
+  ccoptions.addRow(tr, { search: ccoptions.emptySearch, replace: '' })
+  tab.appendChild(tr)
+}
 
-ccoptions.close = function() {
+ccoptions.close = function () {
   // TODO(ark) warn if there are unsaved changes
-  window.close();
-};
+  window.close()
+}
 
-ccoptions.selectTab = function(tabName) {
+ccoptions.selectTab = function (tabName) {
   // each tab is selected with a {tabName}Button and in {tabName}Container
-  var tabs = ['patterns', 'testing'];
+  var tabs = ['patterns', 'testing']
 
   for (var i = 0; i < tabs.length; i++) {
-    var button = document.getElementById(tabs[i] + 'Button').parentNode;
-    var container = document.getElementById(tabs[i] + 'Container');
-    if (tabs[i] == tabName) {
-      button.className = 'selected';
-      container.style.display = 'block';
-      ccoptions.currentTab = tabName;
+    var button = document.getElementById(tabs[i] + 'Button').parentNode
+    var container = document.getElementById(tabs[i] + 'Container')
+    if (tabs[i] === tabName) {
+      button.className = 'selected'
+      container.style.display = 'block'
+      ccoptions.currentTab = tabName
     } else {
-      button.className = '';
-      container.style.display = 'none';
+      button.className = ''
+      container.style.display = 'none'
     }
   }
-};
+}
 
-ccoptions.test = function() {
-  var input = document.getElementById('input').value;
-  var pat = copyclipper.decodeRegexes(document.getElementById('regexes').value);
-  var output = copyclipper.replace(input, pat);
-  document.getElementById('output').innerHTML = copyclipper.textToHtml(output);
-};
+ccoptions.test = function () {
+  var input = document.getElementById('input').value
+  var pat = copyclipper.decodeRegexes(document.getElementById('regexes').value)
+  var output = copyclipper.replace(input, pat)
+  document.getElementById('output').innerHTML = copyclipper.textToHtml(output)
+}
 
-ccoptions.intervalFunction = function() {
+ccoptions.intervalFunction = function () {
   switch (ccoptions.currentTab) {
   // case 'patterns': ccoptions.patternsInterval(); break;
-  case 'testing': ccoptions.testingInterval(); break;
+    case 'testing': ccoptions.testingInterval(); break
   }
-};
+}
 
-ccoptions.testingInterval = function() {
-  var els = ['clipboardValue', 'clipboardValueOriginal'];
+ccoptions.testingInterval = function () {
+  var els = ['clipboardValue', 'clipboardValueOriginal']
   for (var i = 0; i < els.length; i++) {
-    document.getElementById(els[i]).innerText = copyclipper[els[i]];
+    document.getElementById(els[i]).innerText = copyclipper[els[i]]
   }
-};
+}
 
-ccoptions.onLoad = function() {
-  copyclipper.restoreRegexes();
-  chrome.storage.sync.get('regexes', function(reply) {
-    ccoptions.restoreRegexes(reply['regexes']);
-  });
-  function getEl(el) { return document.getElementById(el); };
-  getEl('save').addEventListener('click', ccoptions.saveRegexes, true);
-  getEl('close').addEventListener('click', ccoptions.close, true);
+ccoptions.onLoad = function () {
+  copyclipper.restoreRegexes()
+  chrome.storage.sync.get('regexes', function (reply) {
+    ccoptions.restoreRegexes(reply.regexes)
+  })
+  function getEl (el) { return document.getElementById(el) };
+  getEl('save').addEventListener('click', ccoptions.saveRegexes, true)
+  getEl('close').addEventListener('click', ccoptions.close, true)
   getEl('patternsButton').addEventListener('click',
-      function() { ccoptions.selectTab('patterns'); }, true);
+    function () { ccoptions.selectTab('patterns') }, true)
   getEl('testingButton').addEventListener('click',
-      function() { ccoptions.selectTab('testing'); }, true);
+    function () { ccoptions.selectTab('testing') }, true)
 
-  getEl('input').addEventListener('keyup', ccoptions.test, true);
-  getEl('regexes').addEventListener('keyup', ccoptions.test, true);
+  getEl('input').addEventListener('keyup', ccoptions.test, true)
+  getEl('regexes').addEventListener('keyup', ccoptions.test, true)
 
-  var el = getEl('tests');
+  var el = getEl('tests')
   for (var i = 0; i < el.childNodes.length; i++) {
-    var t = el.childNodes[i];
-    if (t.tagName == 'LI') {
-      t.addEventListener('click', function(e) {
-        copyclipper.setClipboardContents(e.target.innerText);
-        copyclipper.copyclip();
-      }, true);
+    var t = el.childNodes[i]
+    if (t.tagName === 'LI') {
+      t.addEventListener('click', function (e) {
+        copyclipper.setClipboardContents(e.target.innerText)
+        copyclipper.copyclip()
+      }, true)
     }
   }
 
   // put the clipboard into 'input'
-  getEl('input').value = copyclipper.getClipboardContents();
-  ccoptions.test();
+  getEl('input').value = copyclipper.getClipboardContents()
+  ccoptions.test()
 
   // ccoptions.selectTab('testing');
-  ccoptions.intervalId = window.setInterval(ccoptions.intervalFunction, 1000);
-};
+  ccoptions.intervalId = window.setInterval(ccoptions.intervalFunction, 1000)
+}
 
-window.addEventListener('DOMContentLoaded', ccoptions.onLoad, false);
+window.addEventListener('DOMContentLoaded', ccoptions.onLoad, false)
