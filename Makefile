2f1d90dbec15cf75d99718feba3f0b8e3d649151
Alex K <arkarkark@users.noreply.github.com>
2019-09-26 10:52:53 -0700

    try new permission events

--- Makefile
+++ Makefile
@@ -1,3 +1,33 @@
+SRC_ICON = copyclipper.png
+ICONS=icon128.png icon48.png icon16.png
+ZIP_EXCLUDE= .git* $(SRC_ICON) screenshot*.png Makefile README.md \
+	node_modules/* package.json dist/* .DS_Store *.xcf \
+	copyclipperrc *.py
+
+JSON_FILES = manifest.json
+JS_FILES = background.js options.js
+
+space :=
+space +=
+ZIP_EXCLUDE_FLAGS = --exclude=$(subst $(space),$(space)--exclude=,$(ZIP_EXCLUDE))
+
+build: setup $(wildcard **/*) $(ICONS) $(JSON_FILES) $(JS_FILES)
+	dirname=$(shell basename $(PWD)); zip -r $(ZIP_EXCLUDE_FLAGS) dist/$$dirname.zip . $(ZIP_INCLUDES)
+
+clean:
+	rm -fv $(JSON_FILES) $(JS_FILES) $(ICONS)
+	rm -rf node_modules/ vendor/ dist/
+	rm -fv icon[1-9][0-9]*.png
+	dirname=$(shell basename $(PWD)); rm -fv dist/$$dirname.zip
+
+icon%.png: $(SRC_ICON)
+	convert $(SRC_ICON) -resize $* $@
+
+dist:
+	mkdir -v dist
+
+setup: dist
+
 python:
 	if ! type -f terminal-notifier; then brew install terminal-notifier; fi
 	if ! python -c "import AppKit"; then \
--- background.js
+++ background.js
@@ -187,7 +187,27 @@ copyclipper.restoreRegexes();
 copyclipper.clipboardValue = '';  // the last value we saw
 copyclipper.clipboardValueOriginal = '';
 
-if (((chrome.clipboard || {}).onClipboardDataChanged || {}).addListener) {
+/*
+navigator.permissions.query({
+  name: 'clipboard-read'
+}).then(permissionStatus => {
+  // Will be 'granted', 'denied' or 'prompt':
+  console.log(permissionStatus.state);
+
+  // Listen for changes to the permission state
+  permissionStatus.onchange = () => {
+    console.log(permissionStatus.state);
+  };
+});
+*/
+
+function getChromeVersion () {
+  var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+  return raw ? parseInt(raw[2], 10) : false;
+}
+
+if (getChromeVersion() > 69 && ((chrome.clipboard || {}).onClipboardDataChanged || {}).addEventListener) {
+  console.log("FINALLY USING NEW onClipboardDataChanged API!")
   // use new api https://developer.chrome.com/apps/clipboard
   chrome.clipboard.onClipboardDataChanged.addListener(copyclipper.pollClipboard)
 } else {
